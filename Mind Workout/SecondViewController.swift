81f0911c86c4ae5613c95ab6add86daa8544ba73
Fejcvk <tomaszfejcak@icloud.com>
2017-05-31 13:14:03 +0200

    Initial Commit

---
+++ Mind Workout/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Mind Workout
+//
+//  Created by Tomasz Fejcak on 31/05/2017.
+//  Copyright © 2017 Tomasz Fejcak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
